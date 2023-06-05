0134a75a75ebaa2bed5c416888f1aa03bd4dad37
MuhJumarawinoto <muhamadjumarawinoto.77@gmail.com>
2023-06-05 16:37:32 +0700

    Layout dasar sudah di pasang

---
+++ resources/views/Home.blade.php
@@ -0,0 +1,8 @@
+@include('layouts.Header.Header')
+<body>
+@include('layouts.Header.HeaderPage')   
+@include('layouts.Body.Sidebar') 
+@include('layouts.Footer.Feature')
+@include('layouts.Footer.Footer')
+@include('layouts.Footer.Copyright') 
+</body>
---
+++ resources/views/layouts/Header/Header.blade.php
@@ -0,0 +1,21 @@
+<head>
+		<title>Iridium by TEMPLATED</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<link href='http://fonts.googleapis.com/css?family=Arimo:400,700' rel='stylesheet' type='text/css'>
+		<!--[if lte IE 8]><script src="js/html5shiv.js"></script><![endif]-->
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+		<script src="{{asset( 'js/skel.min.js') }}"></script>
+
+		<script src="{{asset ('js/skel-panels.min.js')}}"></script>
+		<script src="{{asset('js/init.js')}}"></script>
+		<noscript>
+    <link rel="stylesheet" href="{{asset('css/skel-noscript.css')}}" />
+			<link rel="stylesheet" href="{{asset('css/style.css')}}" />
+			<link rel="stylesheet" href="{{asset('css/style-desktop.css')}}" />
+		</noscript>
+		<!--[if lte IE 8]><link rel="stylesheet" href="css/ie/v8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="css/ie/v9.css" /><![endif]-->
+	
+    </head>
