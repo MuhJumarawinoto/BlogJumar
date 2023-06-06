d95f1a95eadff986fce2f824bc6251200ed8fee1
MuhJumarawinoto <muhamadjumarawinoto.77@gmail.com>
2023-06-06 11:37:24 +0700

    perubahan template

--- resources/views/Home.blade.php
+++
@@ -1,8 +0,0 @@
-@include('layouts.Header.Header')
-<body>
-@include('layouts.Header.HeaderPage')   
-@include('layouts.Body.Sidebar') 
-@include('layouts.Footer.Feature')
-@include('layouts.Footer.Footer')
-@include('layouts.Footer.Copyright') 
-</body>
--- resources/views/layouts/Header/Header.blade.php
+++ resources/views/layouts/Header/Header.blade.php
@@ -1,21 +1,30 @@
 <head>
-		<title>Iridium by TEMPLATED</title>
-		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-		<meta name="description" content="" />
-		<meta name="keywords" content="" />
-		<link href='http://fonts.googleapis.com/css?family=Arimo:400,700' rel='stylesheet' type='text/css'>
-		<!--[if lte IE 8]><script src="js/html5shiv.js"></script><![endif]-->
-		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-		<script src="{{asset( 'js/skel.min.js') }}"></script>
-
-		<script src="{{asset ('js/skel-panels.min.js')}}"></script>
-		<script src="{{asset('js/init.js')}}"></script>
-		<noscript>
-    <link rel="stylesheet" href="{{asset('css/skel-noscript.css')}}" />
-			<link rel="stylesheet" href="{{asset('css/style.css')}}" />
-			<link rel="stylesheet" href="{{asset('css/style-desktop.css')}}" />
-		</noscript>
-		<!--[if lte IE 8]><link rel="stylesheet" href="css/ie/v8.css" /><![endif]-->
-		<!--[if lte IE 9]><link rel="stylesheet" href="css/ie/v9.css" /><![endif]-->
-	
-    </head>
+	      <!-- basic -->
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1"> 
+      <!-- mobile metas -->
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="viewport" content="initial-scale=1, maximum-scale=1"> 
+      <!-- site metas -->
+      <title>Grand Coffee</title>
+      <meta name="keywords" content="">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <!-- bootstrap css -->
+      <link rel="stylesheet" type="text/css" href="{{asset('css/bootstrap.min.css')}}"> 
+      <!-- style css -->
+      <link rel="stylesheet" type="text/css" href="{{asset('css/style.css')}}"> 
+      <!-- Responsive-->
+      <link rel="stylesheet" href="{{asset('css/responsive.css')}}"> 
+      <!-- fevicon -->
+      <link rel="icon" href="{{asset('images/fevicon.png')}}" type="image/gif" />
+      <!-- Scrollbar Custom CSS -->
+      <link rel="stylesheet" href="{{asset('css/jquery.mCustomScrollbar.min.css')}}">
+      <!-- Tweaks for older IEs-->
+      <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css"> 
+      <!-- owl stylesheets --> 
+      <link rel="stylesheet" href="{{asset('css/owl.carousel.min.css')}}">
+      <!-- <link rel="stylesheet" href="{{asset('css/owl.theme.default.min.css')}}"> -->
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen"> 
+</head>
